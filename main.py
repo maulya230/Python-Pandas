5af959cfd1d533cf84f504c0e5de33076d966373
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-12-03 18:34:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,19 @@ import pandas
 data = pandas.read_csv("weather_data.csv")
 print(data)
 # print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(len(temp_list))
+
+# average=sum(temp_list)/len(temp_list)
+# print(average)
+
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# get data in columns
+#print(data["condition"])
+print(data.condition)
